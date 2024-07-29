a81fb3f1c90e94e7f51c3f190214840516f8c322
Flynt Gao <flynt.gao@consultant.udtrucks.com>
2024-07-29 19:43:00 +0800

    add token

--- src/webparts/vueMock/VueMockWebPart.ts
+++ src/webparts/vueMock/VueMockWebPart.ts
@@ -9,7 +9,7 @@ import router from '../../router';
 
 
 
-
+export let mytoken = ""
 export interface IVueMockWebPartProps {
   description: string;
   webpartcolor?: string;
@@ -17,6 +17,7 @@ export interface IVueMockWebPartProps {
 
 export default class VueMockWebPart extends BaseClientSideWebPart<IVueMockWebPartProps> {
 
+  protected token: string = null;
   public render(): void {
     this.domElement.innerHTML = `<div  id="app-${this.context.instanceId}"></div>`
     const app = createApp(AppComponent);
@@ -25,6 +26,13 @@ export default class VueMockWebPart extends BaseClientSideWebPart<IVueMockWebPar
     app.mount(`#app-${this.context.instanceId}`);
   }
   protected onInit(): Promise<void> {
+    this.context.aadTokenProviderFactory.getTokenProvider().then((provider): void => {
+      provider.getToken('b407b2b3-b500-4ea9-92f1-ca4c28558347').then((token): void => {
+        this.token = token;
+        mytoken = token
+        console.log("tokenAAD:" + token);
+      }, err => console.log("errorTokenAAD:" + err));
+    }, err => console.log("errorGetProvider:" + err));
     return super.onInit();
   }
 
